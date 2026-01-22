c0f1e08bfcbdc958efb605da44c18364a6821425
Leo Dong <ldong@mcad.edu>
2026-01-22 02:01:53 -0600

    Upload WEEK1 ex2 files

---
+++ week1/ex2/main.js
@@ -0,0 +1,18 @@
+
+let canvasWidth = parseInt(
+    prompt("Input a number between 400 and 600"), 10
+);
+let canvasHeight = parseInt(
+    prompt("Input a number between 400 and 600"), 10
+);
+
+function setup() {
+    createCanvas(canvasWidth,canvasHeight);
+}
+
+function draw() {
+    background(220);
+}
+
+
+
