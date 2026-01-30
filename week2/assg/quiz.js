68574371c5a5f3073ce4caa10295c01fbb351f65
Leo Dong <ldong@mcad.edu>
2026-01-29 23:11:54 -0600

    Complete the assignment codes

--- week2/assg/quiz.js
+++ week2/assg/quiz.js
@@ -1,17 +1,23 @@
-// Array of game objects
-const myGames = [
-    { title: 'Minecraft', type: 'Survival', numberOfPlayers: '1-Multiplayer', rating: '9' },
-    { title: 'Breath of the Wild', type: 'Adventure', numberOfPlayers: '1', rating: '10' },
-    { title: 'Among Us', type: 'Party', numberOfPlayers: '4-15', rating: '8' }
+// Array of questions and anwers
+const natureQuiz = [
+    { question: 'What gas do plants absorb from the air to make food?', answer: 'Carbon Dioxide' },
+    { question: 'What process do plants use to make their own food using sunlight?', answer: 'Photosynthesis' },
+    { question: 'What do we call animals that eat only plants?', answer: 'Herbivores' },
+    { question: 'What is the main source of energy for Earth?', answer: 'The Sun' },
+    { question: 'What natural resource is needed for all living things to survive?', answer: 'Water' },
+    { question: 'What layer of the Earth is made of soil and rock where plants grow?', answer: 'The Crust' },
+    { question: 'What do we call trash that can be reused to make new products?', answer: 'Recyclables' },
+    { question: 'What gas do humans breathe in to stay alive?', answer: 'Oxygen' },
+    { question: 'What do we call a place where plants and animals live?', answer: 'Habitat' },
+    { question: 'What type of energy comes from wind, sun, or water and does not run out?', answer: 'Renewable energy' }
 ];
 
-// Prompt the user and cast input to a number
-let gameIdx = Number(
-    prompt("I have 3 games in my collection. Pick a number between 1 and 3 and I'll tell you about that game ")
-);
+// Prompt a random question
+const randomNumber = Math.round(Math.random() * (natureQuiz.length - 1));
+prompt('Please the following question: ', natureQuiz[randomNumber-1].question);
 
-// Alert the selected game using string concatenation
+// Alert the selected question and answer
 alert(
-    'You selected ' + myGames[gameIdx-1].title + ', which is a ' + myGames[gameIdx-1].type + ' game for ' + myGames[gameIdx-1].numberOfPlayers + ' player(s). It has a rating of ' + myGames[gameIdx-1].rating + '.' 
+    'You answered. The correct answer was ' + natureQuiz[randomNumber-1].answer + '.' 
 );
 
