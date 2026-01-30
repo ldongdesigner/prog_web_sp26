8f6c59eb98f71bcd760a210e5088b0f30437bc38
Leo Dong <ldong@mcad.edu>
2026-01-29 23:29:46 -0600

    Finalize week2 assignment

--- week2/assg/quiz.js
+++ week2/assg/quiz.js
@@ -1,23 +1,25 @@
-// Array of questions and anwers
+// Array of questions and answers
 const natureQuiz = [
-    { question: 'What gas do plants absorb from the air to make food?', answer: 'Carbon Dioxide' },
+    { question: 'What gas do plants absorb from the air to make food?', answer: 'Carbon dioxide' },
     { question: 'What process do plants use to make their own food using sunlight?', answer: 'Photosynthesis' },
     { question: 'What do we call animals that eat only plants?', answer: 'Herbivores' },
     { question: 'What is the main source of energy for Earth?', answer: 'The Sun' },
     { question: 'What natural resource is needed for all living things to survive?', answer: 'Water' },
-    { question: 'What layer of the Earth is made of soil and rock where plants grow?', answer: 'The Crust' },
+    { question: 'What layer of the Earth is made of soil and rock where plants grow?', answer: 'The crust' },
     { question: 'What do we call trash that can be reused to make new products?', answer: 'Recyclables' },
     { question: 'What gas do humans breathe in to stay alive?', answer: 'Oxygen' },
     { question: 'What do we call a place where plants and animals live?', answer: 'Habitat' },
     { question: 'What type of energy comes from wind, sun, or water and does not run out?', answer: 'Renewable energy' }
 ];
 
-// Prompt a random question
-const randomNumber = Math.round(Math.random() * (natureQuiz.length - 1));
-prompt('Please the following question: ', natureQuiz[randomNumber-1].question);
+// Pick a random question
+const randomIndex = Math.round(Math.random() * (natureQuiz.length - 1));
 
-// Alert the selected question and answer
+// Prompt the user with the question 
+const userResponse = prompt(natureQuiz[randomIndex].question);
+
+// Show the response and the correct answer
 alert(
-    'You answered. The correct answer was ' + natureQuiz[randomNumber-1].answer + '.' 
+    'You answered ' + userResponse + '. The correct answer was ' + natureQuiz[randomIndex].answer + '.' 
 );
 
