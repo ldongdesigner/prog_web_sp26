605ba850e63f83ca88a76030a2689d8478c17c7a
Leo Dong <ldong@mcad.edu>
2026-02-04 01:10:26 -0600

    Complete Ex1

--- week3/ex1/tile.js
+++ week3/ex1/tile.js
@@ -1,30 +1,48 @@
+const primaryColors = ['#1B5E20', '#2E7D32', '#E0F2F1'];
+const secondaryColors = ['#26A69A', '#42A5F5', '#FFCA28', '#66BB6A', '#80DEEA'];
+const tertiaryColors = ['#004D40', '#0277BD', '#558B2F', '#F9A825', '#5D4037',  '#00838F', '#37474F'];
+let primaryColor, secondaryColor, tertiaryColor;
+
 function setup() {
     createCanvas(200, 200);
 }
 
-function createTile() {
+function drawTile() {
     translate(0, 0);
-    fill('blue');
-    rect(0, 0, 200, 200);
-    stroke('white');
-    strokeWeight(5);
-    line(0, 0, 200, 200);
-    line(200, 0, 0, 200);
 
+    // Pick one background color per run
+    primaryColor = primaryColors[int(random(primaryColors.length))];
     noStroke();
-    fill('yellow');
-    circle(100, 50, 50);
-    circle(50, 100, 50);
-    circle(150, 100, 50);
-    circle(100, 150, 50);
+    fill(primaryColor);
+    rect(0, 0, 200, 200);
+    
 
-    noFill();
-    stroke('purple');
-    strokeWeight(5);
-    circle(100, 100, 100);
+    for (let boxX = 20; boxX <= 180; boxX += 20) {
+        for (let boxY = 20; boxY <= 180; boxY +=20) {
+            
+            // One line per grid point
+            tertiaryColor = tertiaryColors[int(random(tertiaryColors.length))];
+            stroke(tertiaryColor);
+            strokeWeight(1);
+            line (boxX, boxY, 100, 100);
+
+            // Draw concentric circles per grid point
+            for (let boxSize = 2; boxSize <= 15; boxSize +=4) {
+                secondaryColor = secondaryColors[int(random(secondaryColors.length))];
+                // noStroke();
+                // fill(secondaryColor);
+                noFill();
+                stroke(secondaryColor);
+                strokeWeight(1);
+                circle(boxX, boxY, boxSize);
+            }
+            
+        }
+    }
 }
 
 function draw() {
-    createTile();
+    drawTile();
+    noLoop();
 }
 
