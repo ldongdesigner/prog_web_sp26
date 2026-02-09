c333b793f076cc8aa3cbb84cc0886e11833b1ae8
Leo Dong <ldong@mcad.edu>
2026-02-09 13:16:36 -0600

    1st draft ex1

---
+++ week4/ex1/DOM.js
@@ -0,0 +1,21 @@
+// select empty div and assign to a variable
+const myEmptyDiv = document.querySelector('#myEmptyDiv');
+
+// put an hi inside of it
+// create a new element
+const heading = document.createElement('h1');
+heading.innerHTML = 'Hello class! It is a snowy day!';
+heading.style.cursor = 'pointer';
+heading.addEventListener('click', handleHeadingClick);
+myEmptyDiv.appendChild(heading);
+function handleHeadingClick() {
+    // console.log('Heading has been clicked');
+    document.body.style.backgroundColor = 'magenta';
+}
+
+
+
+
+
+
+
