b2f319a08f154404b8dc28b29a92400ef889880b
Leo Dong <ldong@mcad.edu>
2026-02-12 22:06:16 -0600

    Update events.js

--- week4/ex2/events.js
+++ week4/ex2/events.js
@@ -1,38 +1,54 @@
-const rectWidth = 75;
-const rectHeight = 75;
-let speed;
+// Clicker counter (p5.js)
+
+const rectSize = 75;
 let rectX = 0;
-let rectY;
+let rectY = 0;
+let speed = 0;
 
 let clickCount = 0;
+let bgColor; // Store background color
 
 function setup() {
     createCanvas(500, 500);
-    rectY = random(height - rectHeight);
+    rectY = random(0, height * 0.25 - rectSize);
     speed = random(1, 3);
+    bgColor = 155; // Initial background color
 }
 
 function draw() {
-    background(155);
+    background(bgColor);
+
     drawShape();
+
     rectX += speed;
     rectY += speed;
 
     if (rectX > width) {
         noLoop();
-        text('Your score was ' + clickCount, 100, 300);
+        textSize(24);
+        fill(255); // White
+        textStyle(BOLD);
+        text('Your score was ' + clickCount + ' !', 150, 250);
     }
 }
 
 function mousePressed() {
-    if ((mouseX >= rectX && mouseX <= rectX + rectWidth) && (mouseY >= rectY && mouseY <= rectY + rectHeight)) {
+    // Detec click inside square
+    if (
+        mouseX >= rectX && 
+        mouseX <= rectX + rectSize && 
+        mouseY >= rectY && 
+        mouseY <= rectY + rectSize
+    ) {
         clickCount++;
-        console.log('hit', clickCount);
+
+        // Change background to random color
+        bgColor = color(random(255), random(255), random(255));
     }
 }
 
 function drawShape() {
     fill('purple');
-    rect(rectX, rectY, rectWidth, rectHeight);
+    rect(rectX, rectY, rectSize, rectSize);
 }
 
