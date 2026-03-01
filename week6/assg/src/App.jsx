9c05d8304ad3360d5bafb50c9bf9208810596693
Leo Dong <ldong@mcad.edu>
2026-02-28 23:11:09 -0600

    Refine assg

--- week6/assg/src/App.jsx
+++ week6/assg/src/App.jsx
@@ -2,30 +2,33 @@ import './App.css'
 
 function App() {
 
-  const llamaFacts = [
+  const baseFacts = [
     "Llamas are domesticated camelids.",
     "Llamas come from South America.",
     "Llamas are used as pack animals.",
     "Llamas eat grass and hay.",
     "Llamas can spit when threatened.",
-    "Llamas can be trained to dance.",
-    "Llamas cannot be trained to dance.",
   ];
 
+  const danceFacts = {
+    yes: "Llamas can be trained to dance.",
+    no: "Llamas cannot be trained to dance.",
+  };
+  
   const canDance = false;
 
-  const baseFacts = llamaFacts.slice(0, 5);
-  const danceFact = canDance ? llamaFacts[5] : llamaFacts[6];
+  const danceFact = canDance ? danceFacts.yes : danceFacts.no;
 
   return (
    <div className="container">
     <h1>Llama Facts</h1>
+
     <ul>
-      {baseFacts.map((fact, index) => (
-        <li key={index}>{fact}</li>
+      {baseFacts.map((fact) => (
+        <li key={fact}>{fact}</li>
       ))}
 
-      <li>{danceFact}</li>
+      <li key="dance-fact">{danceFact}</li>
     </ul>
    </div>
   );
