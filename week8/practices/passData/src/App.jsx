e3edbd4e1e70250d774ef7be7e2bf43ee49601b9
Leo Dong <ldong@mcad.edu>
2026-03-09 20:43:16 -0500

    week8 assg

--- week8/practices/passData/src/App.jsx
+++ week8/practices/passData/src/App.jsx
@@ -1,14 +1,38 @@
 import { useState } from 'react'
 
 import './App.css'
+import UsStateComponent from './UsStateComponent';
 
 function App() {
  const [headline, setHeadline] = useState("List of American States");
- const [usStates, setUsStates] = useState();
+ const [usStates, setUsStates] = useState([
+  "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
+"Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+"Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+"New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+"South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
+]);
 
+function deleteState(stateName) {
+  const updatedArray = usStates.filter((state) => {
+    return state !== stateName;
+  });
+  setUsStates(updatedArray);
+}
+
+function focusState(stateName) {
+  setHeadline(stateName)
+}
   return (
     <>
-      <h1>{headline}</h1>
+      <h1>{`${headline} is the focus`}</h1>
+      {usStates.map((myState) => {
+        return <UsStateComponent 
+        key={myState} 
+        stateName={myState}
+        deleteFn={deleteState}
+        focusFn={focusState} />
+      })}
     </>
   )
 }
